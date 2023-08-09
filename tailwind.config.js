e04b529deada1a06173432ba6207998d56a1c6ba
vicsnet <adesanmiadebisi@gmail.com>
2023-08-09 15:02:49 +0100

    features added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,26 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      screens: {
+        bigDesktop: { max: "3200px" }, //Big desktop
+        semiBigDesktop: { max: "1920px" },
+        bigScreen:{max: "1539"},
+        bSemiBig: { max: "1440px" }, //1440
+        bSemismall: { max: "1366px" }, //
+        lgDesktop: { max: "1280px" }, //large desktop
+        smDesktop: { max: "1024px" }, // small desktop
+        smDesk: { max: "917px" }, // small desktop
+        tabletAir: { max: "820px" }, // big tablet
+        tablet: { max: "768px" },
+        surfDuo:{max:"540"},
+        mobile: { max: "480px" },
+        smMobile: { max: "320px" },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
