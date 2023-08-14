b4b1a99dc9ea212caf80c6b3ee3966d354cf8de2
dunnie99 <oluwadunnirachael@gmail.com>
2023-08-14 13:24:08 +0100

    ht

--- features/Home/Homefeatures.jsx
+++ features/Home/Homefeatures.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Homefeatures() {
   return (
     <main>
-      <div><h1>My Photos</h1></div>
+      <h1 className=''>My Photos</h1>
+      
+      <div className=''>
+        <h3>547 Photo Uploaded</h3>
+        <h3>9353 Photo Space left</h3>
+        <h3>Basic Plan</h3>
+      </div>
+
+      <div className=''>
+
+      </div>
       
     </main>
   )
