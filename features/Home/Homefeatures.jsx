d9124b1daee9a985b2b4b9d1275003032d4ac050
vicsnet <adesanmiadebisi@gmail.com>
2023-08-18 15:38:14 +0100

    No Photos Adjusted

--- features/Home/Homefeatures.jsx
+++ features/Home/Homefeatures.jsx
@@ -1,44 +1,14 @@
 import React from 'react';
 import Allphotos from "./components/Allphotos";
 import NoPhoto from "./components/NoPhoto";
+import HomeWithPhotos from './components/HomeWithPhotos';
 
 export default function Homefeatures() {
   return (
     <main className="items-center side">
       
 
-      {/*<h4 className="text-[32px] tracking-[0.25%] text-justify font-normal leading-30 gap-10 pl-[70px] pt-[10px] side tablet:text-[28px] mobile:text-[20px] mobile:pl-[20px]">My Photos</h4>
-    
-      
-
-       <div className='flex flex-wrap gap-4 w-[93%] smDesktop:w-[90%] mx-auto font-medium text-[17px] tabletAir:text-[14px] leading-7 tracking-[0.5%] text-justify font-Space-Grotesk border-[#C5CBFD] border-[1px] rounded-[8px]  mt-[20px] py-[16px] ml-[34px] mobile:hidden'>
-        <div className='w-[95%] mx-auto flex gap-6 '>
-        <h3 className=''>547 Photo Uploaded</h3>
-        <h3>9353 Photo Space left</h3>
-        <h3>Basic Plan</h3>
-        </div>
-
-      </div>
-      <div className="hidden mobile:flex mobile:justify-between w-[90%] mx-auto mt-6">
-          <div className="flex gap-2">
-
-          <img src="/../Icon/loop.svg" alt="" />
-          <h2 className="">647</h2>
-          </div>
-          <div className="flex gap-2">
-
-          <img src="/../Icon/space.svg" alt="" />
-          <h2 className="">9353</h2>
-          </div>
-          <div className="flex gap-2">
-
-          <img src="/../Icon/plan.svg" alt="" />
-          <h2 className="capitalize">basic</h2>
-          </div>
-        </div>
-      <div className='ml-[10px] mobile:ml-[20px] '>
-      <Allphotos />
-      </div> */}
+{/* <HomeWithPhotos/> */}
 
       <NoPhoto />
       
--- features/Home/components/NoPhoto.jsx
+++ features/Home/components/NoPhoto.jsx
@@ -4,14 +4,14 @@ export default function NoPhoto() {
   return (
     <main>
         <section className="h-[672px] top-[288px] gap-12" >
-            <div className="mx-auto flex justify-center items-center w-[456px] mobile:mt-16 mobile:pr-20">
+            <div className="mx-auto flex justify-center items-center w-[456px] mobile:mt-16 mobile:px-auto mobile:w-[100%]">
             <img src="/../images/guy2.svg" className="w-[456px] mobile:w-[280px]"/>
 
             </div>
 
             <div className="">
-                <h2 className="text-center font-[400] text-[24px] leading-9 mt-2  tablet:mt-[60px] side">No Photo Uploaded.</h2>
-                <h2 className="text-center font-[400] text-[24px] leading-9 mt-8 side">Create a Folder to upload.</h2>
+                <h2 className="text-center font-[400] text-[24px] leading-9 mt-2  tablet:mt-[60px] mobile:text-[20px] side">No Photo Uploaded.</h2>
+                <h2 className="text-center font-[400] text-[24px] mobile:text-[20px] leading-9 mt-8 side">Create a Folder to upload.</h2>
             </div>
             
         </section>
