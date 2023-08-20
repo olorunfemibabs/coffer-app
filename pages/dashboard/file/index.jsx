400586bf3e75fbfbf06cc449bdfa601ab8c12d6f
akinbola247 <akinbolakehinde247@gmail.com>
2023-08-20 09:43:07 +0100

    progress

--- pages/dashboard/file/index.jsx
+++ pages/dashboard/file/index.jsx
@@ -6,6 +6,8 @@ import { useContractRead, useAccount } from 'wagmi'
 import contractAbi from '../../../utils/cofferabi.json'
 import { useEffect } from "react"
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ethers } from 'ethers';
+
 
 
  
@@ -18,19 +20,35 @@ export default function File() {
       functionName: 'getFolders',
       args: [address]
     })
+
+  const provider = new ethers.JsonRpcProvider("https://eth-sepolia.g.alchemy.com/v2/5ShvcS43c_Wrsfk_jTMZOU0sXXBKaVXP");
+  const contractAddress = "0xa5f2f0648520709c7d1a343227f32028f47E5D5C";
+
+  // const getPhotoNumber = async () => {
+  //   const contract = new ethers.Contract(
+  //     contractAddress,
+  //     contractAbi,
+  //     provider
+  //   );
+  //     try {
+  //       const getFolderDetail = await contract.Getfolder();
+  //     } catch (error) {
+        
+  //     }
+  // }
     
-    // const { data : counterData, isError: counterError, isLoading: isCounterLoading, isSuccess: isCounterSuccess } = useContractRead({
-    //   address: '0xa5f2f0648520709c7d1a343227f32028f47E5D5C',
-    //   abi: contractAbi,
-    //   functionName: 'Getfolder',
-    //   args: [address]
-    // })
+    const { data : counterData, isError: counterError, isLoading: isCounterLoading, isSuccess: isCounterSuccess } = useContractRead({
+      address: '0xa5f2f0648520709c7d1a343227f32028f47E5D5C',
+      abi: contractAbi,
+      functionName: 'mySharedURI',
+      args: [address]
+    })
 
   useEffect(() => {
-    if(isSuccess){
-      // console.log(data);
+    if(isCounterSuccess){
+      console.log(counterData);
     }
-  }, [folderArrayData, isSuccess])
+  }, [folderArrayData, isSuccess, counterData, isCounterSuccess])
   
   return (
     <Layout>
@@ -39,6 +57,7 @@ export default function File() {
                 <ConnectButton />
                <div className="flex flex-wrap w-[1180px] space-x-4">
                 {folderArrayData.map((item, index) =>{
+
                     return <Link href={`file/${item}`} ><Card key={index} folderName={item.name}/></Link>
                 })}
                   <Createfolder />
